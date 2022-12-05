7aea66861a94d3fe9bbe63ef136abe053c768fe6
Lin Yudong <54970185+Campfireman0314@users.noreply.github.com>
2022-12-05 17:13:27 +0100

    Update training_pipeline.py

--- training_pipeline.py
+++ training_pipeline.py
@@ -139,22 +139,3 @@ kwargs = {
     "tags": "hf-asr-leaderboard",
 }
 trainer.push_to_hub(**kwargs)
-
-from transformers import pipeline
-import gradio as gr
-
-pipe = pipeline(model="Campfireman/whisper-small-hi")  # change to "your-username/the-name-you-picked"
-
-def transcribe(audio):
-    text = pipe(audio)["text"]
-    return text
-
-iface = gr.Interface(
-    fn=transcribe, 
-    inputs=gr.Audio(source="microphone", type="filepath"), 
-    outputs="text",
-    title="Whisper Small Swedish",
-    description="Realtime demo for Swedish speech recognition using a fine-tuned Whisper small model.",
-)
-
-iface.launch()#upload the model to huggingface
